3adf951f4f813eb4ea5f8632cdd78242de2a8b41
genta.ameku <genta.ameku@gmail.com>
2018-09-19 08:24:25 +0900

    feat: pick method

--- src/Code.ts
+++ src/Code.ts
@@ -1,9 +1,12 @@
+const SPREADSHEETID = '1JiaOSinQSYaQLJkNsFuMHP98xMbMPdMG15eU-S4ynpU';
+const SHEETNAME = 'register';
+
 const trainStrToObject = (trainNames: string): Train[] => {
   if (typeof trainNames !== 'string') {
     return [];
   }
   const trainsArray: string[] = trainNames.split(',');
-  return trainsArray.map((trainInfo) => {
+  return trainsArray.map(trainInfo => {
     const name = trainInfo.slice(0, trainInfo.indexOf('/'));
     const company = trainInfo.slice(trainInfo.indexOf('/') + 1);
     return { name, company };
@@ -19,21 +22,27 @@ const mapToRow = (
     return { name: name[0], trains: trainStrToObject(targetTrains) };
   });
 
-// const pickTrains = (delayTrains: DelayTrain[] = [], user: User) => {
-//   user.trains.forEach(train => {
-//     const hoge = delayTrains.find(delayTrain => {
-//       return (delayTrain.name === targetTrainName && delayTrain.company === targetTrainCompany);
-//     })
-//   });
-// };
-
-const fetchSpreadSheet = () => {
-  const SPREADSHEETID = '1JiaOSinQSYaQLJkNsFuMHP98xMbMPdMG15eU-S4ynpU';
-  const SHEETNAME = 'register';
-  return SpreadsheetApp.openById(SPREADSHEETID).getSheetByName(SHEETNAME);
+const pickDelayTrains = (delayTrains: Train[] = [], user: User): Train[] => {
+  const results = [];
+  user.trains.forEach(train => {
+    const foundTrains = delayTrains.find(
+      delayTrain =>
+        delayTrain.name === train.name && delayTrain.company === train.company,
+    );
+    results.push(foundTrains);
+  });
+  return results;
 };
 
-const parseSpreadSheetToArray = (spreadsheet): User[] => {
+const fetchSpreadSheet = ({
+  id,
+  sheetName,
+}: {
+  id: string;
+  sheetName: string;
+}) => SpreadsheetApp.openById(id).getSheetByName(sheetName);
+
+const createUsersFromSpreadSheet = (spreadsheet): User[] => {
   const nameColumn = spreadsheet
     .getRange(2, 1, spreadsheet.getLastRow() - 1)
     .getValues();
@@ -45,6 +54,9 @@ const parseSpreadSheetToArray = (spreadsheet): User[] => {
 
 function startSearchDelayTrains() {
   const delayTrains = fetchDelayTrains();
-  const spreadSheet = fetchSpreadSheet();
-  const users = parseSpreadSheetToArray(spreadSheet);
+  const spreadSheet = fetchSpreadSheet({
+    id: SPREADSHEETID,
+    sheetName: SHEETNAME,
+  });
+  const users = createUsersFromSpreadSheet(spreadSheet);
 }
